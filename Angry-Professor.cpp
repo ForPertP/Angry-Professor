e6822149e2c7bc9b3f487ba05ab9eb1f997e19a1
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-12-19 03:25:48 +0900

    Update Angry-Professor.cpp

--- Angry-Professor.cpp
+++ Angry-Professor.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
 string angryProfessor(int k, vector<int> a)
 {
     int count = 0;
